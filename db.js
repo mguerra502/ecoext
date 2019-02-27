617b04d3661b2d38359c5c194f6039f474a9a52c
Gabriel Oliveira <ras.vibpositive@gmail.com>
2019-02-27 16:39:14 +0000

    Adds support to .env

--- db.js
+++ db.js
@@ -1,15 +1,43 @@
 const { Sequelize } = require('sequelize');
+const {
+  ECOEXT_MYSQL_PORT,
+  ECOEXT_MYSQL_HOST,
+  ECOEXT_DATABASE,
+  ECOEXT_DATABASE_USER,
+  ECOEXT_DATABASE_ROOTPASSWORD
+} = require("./utils/config")
 
+// let HOST     = process.env.os == "Windows_NT" ? "localhost" : process.env.ECOEXT_MYSQL_HOST;
+// let DATABASE = process.env.os == "Windows_NT" ? "ecoext" : process.env.ECOEXT_DATABASE;
+// let USERNAME = process.env.os == "Windows_NT" ? "gabriel" : process.env.ECOEXT_DATABASE_USER;
+// let PASSWORD = process.env.os == "Windows_NT" ? "eueeu99" : process.env.ECOEXT_DATABASE_ROOTPASSWORD;
+
+// const Conn = new Sequelize(
+//   {
+//     host: HOST,
+//     database: DATABASE,
+//     username: USERNAME,
+//     password: PASSWORD,
+//     dialect: 'mysql',
+//     port: ECOEXT_MYSQL_PORT
+//   }
+// );
 const Conn = new Sequelize(
   {
-    host: process.env.ECOEXT_MYSQL_HOST,
-    database: process.env.ECOEXT_DATABASE,
-    username: process.env.ECOEXT_DATABASE_USER,
-    password: process.env.ECOEXT_DATABASE_ROOTPASSWORD,
+    host: ECOEXT_MYSQL_HOST,
+    database: ECOEXT_DATABASE,
+    username: ECOEXT_DATABASE_USER,
+    password: ECOEXT_DATABASE_ROOTPASSWORD,
     dialect: 'mysql',
-    port: 3306
+    port: ECOEXT_MYSQL_PORT
   }
 );
+
+console.log(`ECOEXT_DATABASE ${ECOEXT_DATABASE}`)
+console.log(`ECOEXT_DATABASE_USER ${ECOEXT_DATABASE_USER}`)
+console.log(`ECOEXT_DATABASE_ROOTPASSWORD ${ECOEXT_DATABASE_ROOTPASSWORD}`)
+console.log(`ECOEXT_MYSQL_HOST ${ECOEXT_MYSQL_HOST}`)
+console.log(`ECOEXT_MYSQL_PORT ${ECOEXT_MYSQL_PORT}`)
     
 const Purse                 = Conn.import(__dirname + "/db/schema/Purse")
 const Account               = Conn.import(__dirname + "/db/schema/Account")
@@ -22,7 +50,7 @@ const Establishment         = Conn.import(__dirname + "/db/schema/Establishment"
 const Transaction           = Conn.import(__dirname + "/db/schema/Transaction")
 const Notification          = Conn.import(__dirname + "/db/schema/Notification")
 
-Notification.belongsToMany
+// Notification.belongsToMany
 
 Account.belongsToMany(Notification, {through: AccountNotifications, foreignKey: 'account_id'});
 Notification.belongsToMany(Account, {through: AccountNotifications, foreignKey: 'notification_id'});
--- index.js
+++ index.js
@@ -1,20 +1,89 @@
 const Express = require( 'express');
+const bodyParser = require('body-parser')
 const GraphHTTP = require( 'express-graphql');
 const Schema = require( './schema');
-const graphiql = require( 'graphiql');
+// const graphiql = require( 'graphiql');
+
+/*
+const jwt = require('express-jwt');
+const bcrypt = require('bcrypt')
+const jsonwebtoken = require('jsonwebtoken')
+const jwksRsa = require('jwks-rsa')
+/**/
 
 // config
 
-const APP_PORT = 3000;
+const APP_PORT = process.env.ECOEXT_WEB_PORT;
 const app = Express();
 
-app.use('/', GraphHTTP({
+app.use(bodyParser.json())
+
+/*
+// authentication middleware
+const authMiddleware = jwt({
+    // dynamically provide a signing key based on the kid in the header and 
+    // the signing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        // jwksUri: `https://YOUR_AUTH0_DOMAIN/.well-known/jwks.json`
+    }),
+
+    // validate the audience and the issuer.
+    // audience: '{YOUR_API_IDENTIFIER}',
+    // issuer: `https://YOUR_AUTH0_DOMAIN/`,
+    algorithms: ['RS256']
+})
+
+app.use(authMiddleware)
+
+// app.use('/', GraphHTTP({
+    //     schema: Schema,
+    //     pretty: true,
+    //     graphiql: true
+    // }))
+    
+    login (_, { email, password }) {
+        
+        const user = await User.findOne({ where: { email } })
+        
+        if (!user) {
+            throw new Error('No user with that email')
+        }
+        
+        const valid = await bcrypt.compare(password, user.password)
+        
+        if (!valid) {
+            throw new Error('Incorrect password')
+        }
+        
+        // return json web token
+        return jsonwebtoken.sign({
+            id: user.id,
+            email: user.email
+        }, 'somesuperdupersecret', { expiresIn: '1y' })
+    }
+    
+app.use('/api', GraphHTTP(req => ({
+    schema: Schema,
+    pretty: true,
+    graphiql: true,
+    context: {
+        user: req.user
+    }
+})))
+
+/**/
+
+
+app.use('/api', GraphHTTP(req => ({
     schema: Schema,
     pretty: true,
     graphiql: true
-}))
+})))
 
 app.listen(APP_PORT, () => {
-    console.log(process.env);
+    // console.log(process.env);
     console.log(`App listerning on port ${APP_PORT}`)
 })
